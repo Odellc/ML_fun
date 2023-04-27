f8e6a9b9d9db9ae3cd9aa0d63389c2cfdfe843e1
Odellc <codell03@gmail.com>
2023-04-27 10:40:05 -0600

    add linear regression

---
+++ Linear-Regression-PyTorch/LR_PyTorch.py
@@ -0,0 +1,48 @@
+import torch
+from torch.autograd import Variable
+
+
+x_data = Variable(torch.Tensor([[1.0], [2.0], [3.0]]))
+y_data = Variable(torch.Tensor([[2.0], [4.0], [6.0]]))
+
+
+class LinearRegressionModel(torch.nn.Module):
+
+	def __init__(self):
+		super(LinearRegressionModel, self).__init__()
+		self.linear = torch.nn.Linear(1, 1) # One in and one out
+
+	def forward(self, x):
+		y_pred = self.linear(x)
+		return y_pred
+
+
+
+lr_model = LinearRegressionModel()
+
+
+criterion = torch.nn.MSELoss(size_average = False)
+optimizer = torch.optim.SGD(lr_model.parameters(), lr = 0.01)
+
+
+
+for epoch in range(500):
+
+	# Forward pass: Compute predicted y by passing
+	# x to the model
+	pred_y = lr_model(x_data)
+
+	# Compute and print loss
+	loss = criterion(pred_y, y_data)
+
+	# Zero gradients, perform a backward pass,
+	# and update the weights.
+	optimizer.zero_grad()
+	loss.backward()
+	optimizer.step()
+	print('epoch {}, loss {}'.format(epoch, loss.item()))
+
+
+new_var = Variable(torch.Tensor([[4.0]]))
+pred_y = lr_model(new_var)
+print("predict (after training)", 4, lr_model(new_var).item())
